84345183aae9eaba4f7ea009a8510658d5792af2
Brian Powell <bapowell00@gmail.com>
2022-03-07 23:56:46 -0800

    Updated configs

--- .config/fish/completions/et.fish
+++ .config/fish/completions/et.fish
@@ -1,2 +1,1 @@
 complete -c et -f -w "ssh"
-
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,30 +2,23 @@ function cl -d "cd into directory and ls"
   cd $argv
   ls
 end
+function mkdir -d "Create a directory and set CWD"
+    command mkdir $argv
+    if test $status = 0
+        switch $argv[(count $argv)]
+            case '-*'
+
+            case '*'
+                cd $argv[(count $argv)]
+                return
+        end
+    end
+end
 abbr -a l ls -lha
 abbr -a lblk lsblk --output NAME,SIZE,RM,FSTYPE,FSUSE%,SERIAL,MOUNTPOINT
-
 abbr -a reload source ~/.config/fish/config.fish
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
-set -x ANDROID_HOME "/Users/"(whoami)"/Library/Android/sdk"
-set -x ANDROID_SDK_ROOT "/Users/"(whoami)"/Library/Android/sdk"
 
 fish_add_path /usr/local/bin
 fish_add_path $HOME/.local/bin
-fish_add_path $ANDROID_HOME/tools
-fish_add_path $ANDROID_HOME/tools/bin
-fish_add_path $ANDROID_HOME/platform-tools
-fish_add_path $HOME/Library/Developer/flutter/bin
-fish_add_path $HOME/.fastlane/bin
-fish_add_path $HOME/.gem/ruby/2.6.0/bin
-fish_add_path $HOME/go/bin
-
-set -g fish_user_paths "/usr/local/sbin" "/usr/local/opt/ruby/bin" $fish_user_paths
 
 set -Ux EDITOR vim
-
-#set -x fish_emoji_width 2
-#set -x fish_ambiguous_width 2
-
