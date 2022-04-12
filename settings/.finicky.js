9b526a4132a741ee2943a89223aac622c2e4c0e8
Brian Powell <brian@powell.place>
2022-04-12 14:56:20 -0700

    More Config'

---
+++ settings/.finicky.js
@@ -0,0 +1,19 @@
+// ~/.finicky.js
+
+module.exports = {
+  defaultBrowser: "Firefox",
+  handlers: [
+    {
+      // Open apple.com urls in Safari
+      match: ["apple.com/*", "*.apple.com/*"],
+      browser: "Safari"
+    },
+    {
+      // Open *.google.com urls in Google Chrome
+      match: [
+        "meet.google.com/*", // match google.com subdomains
+      ],
+      browser: "Google Chrome"
+    }
+  ]
+};
