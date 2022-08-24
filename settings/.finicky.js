383c7aab7616ad9289b233629403854c1444979c
Brian Powell <brian@powell.place>
2022-08-24 13:54:43 -0700

    Finicky Update

--- settings/.finicky.js
+++ settings/.finicky.js
@@ -5,13 +5,14 @@ module.exports = {
   handlers: [
     {
       // Open apple.com urls in Safari
-      match: ["apple.com/*", "*.apple.com/*"],
+      match: ["apple.com/*", "apple.com", "*.apple.com/*", "*.apple.com"],
       browser: "Safari",
     },
     {
       // Open *.google.com urls in Google Chrome
       match: [
         "meet.google.com/*", // match google.com subdomains
+        "meet.google.com",
       ],
       browser: "Google Chrome",
     },
