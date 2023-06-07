391e59bc39ddc564bee383eab0e382e45f36f882
Brian Powell <brian@powell.place>
2023-06-06 22:54:17 -0700

    Updated finicky

--- settings/.finicky.js
+++ settings/.finicky.js
@@ -1,20 +1,32 @@
 // ~/.finicky.js
 
 module.exports = {
-  defaultBrowser: "Firefox",
+  defaultBrowser: 'Firefox',
   handlers: [
     {
       // Open apple.com urls in Safari
-      match: ["apple.com/*", "apple.com", "*.apple.com/*", "*.apple.com"],
-      browser: "Safari",
+      match: ['apple.com/*', 'apple.com', '*.apple.com/*', '*.apple.com'],
+      browser: 'Safari',
     },
     {
       // Open *.google.com urls in Google Chrome
       match: [
-        "meet.google.com/*", // match google.com subdomains
-        "meet.google.com",
+        'meet.google.com/*', // match google.com subdomains
+        'meet.google.com',
       ],
-      browser: "Google Chrome",
+      browser: 'Google Chrome',
+    },
+    {
+      match: ['*.zoom.us/j/*'],
+      browser: '/Applications/zoom.us.app',
+    },
+    {
+      match: ['music.apple.com/*'],
+      browser: '/System/Applications/Spotify.app',
+    },
+    {
+      match: ['open.spotify.com/*'],
+      browser: '/Applications/Spotify.app',
     },
   ],
 };
